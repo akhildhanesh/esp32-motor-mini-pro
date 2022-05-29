65cb93769bcc769a1691f5266ff1f82c12ae7c27
AKHIL DHANESH <64594716+akhildhanesh@users.noreply.github.com>
2022-05-29 09:02:45 +0530

    Update motor.ino

--- motor.ino
+++ motor.ino
@@ -1,44 +1,33 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
 
-// Replace with your network credentials
-const char* ssid     = "***";
-const char* password = "***";
+const char* ssid     = "*******";
+const char* password = "*******";
 
-// Set web server port number to 80
+const int gasAnalog = 34;
+
 WiFiServer server(80);
 
-// Variable to store the HTTP request
 String header;
 
-// Auxiliar variables to store the current output state
-String output26State = "off";
-String output27State = "off";
-
-// Assign output variables to GPIO pins
 const int motor = 2;
 
-// Set your Static IP address
 IPAddress local_IP(192, 168, 0, 125);
-// Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
 
 IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(8, 8, 8, 8);   //optional
-IPAddress secondaryDNS(8, 8, 4, 4); //optional
+IPAddress primaryDNS(8, 8, 8, 8);  
+IPAddress secondaryDNS(8, 8, 4, 4);
 
 void setup() {
   Serial.begin(115200);
-  // Initialize the output variables as outputs
   pinMode(motor, OUTPUT);
-  // Set outputs to LOW
   digitalWrite(motor, LOW);
 
-  // Configures static IP address
   if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
     Serial.println("STA Failed to configure");
   }
   
-  // Connect to Wi-Fi network with SSID and password
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -46,7 +35,6 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println("IP address: ");
@@ -55,65 +43,66 @@ void setup() {
 }
 
 void loop(){
-  WiFiClient client = server.available();   // Listen for incoming clients
+  WiFiClient client = server.available();  
 
-  if (client) {                             // If a new client connects,
-    Serial.println("New Client.");          // print a message out in the serial port
-    String currentLine = "";                // make a String to hold incoming data from the client
-    while (client.connected()) {            // loop while the client's connected
-      if (client.available()) {             // if there's bytes to read from the client,
-        char c = client.read();             // read a byte, then
-        Serial.write(c);                    // print it out the serial monitor
+//   int analogGas = 999;
+   int analogGas = analogRead(gasAnalog);
+
+   HTTPClient http;
+   String serverPath = "http://192.168.0.217:3000/gas-sensor/" + String(analogGas) + String(digitalRead(motor));
+   http.begin(serverPath.c_str());
+   int httpResponseCode = http.GET();
+
+   if (analogGas >= 800) {
+     digitalWrite(motor, HIGH);
+     HTTPClient http;
+     String serverPath = "http://192.168.0.217:3000/esp32/⚠Gas Intensity is High⚠ =>" + String(analogGas) + "Opening the Window";
+     http.begin(serverPath.c_str());
+     int httpResponseCode = http.GET();
+   }
+
+  if (client) {                            
+    Serial.println("New Client.");       
+    String currentLine = "";                
+    while (client.connected()) {            
+      if (client.available()) {             
+        char c = client.read();           
+        Serial.write(c);                
         header += c;
-        if (c == '\n') {                    // if the byte is a newline character
-          // if the current line is blank, you got two newline characters in a row.
-          // that's the end of the client HTTP request, so send a response:
+        if (c == '\n') {                  
           if (currentLine.length() == 0) {
-            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
             client.println("Connection: close");
             client.println();
             
-            // turns the GPIOs on and off
-            if (header.indexOf("GET /gate/open") >= 0) {
+            if (header.indexOf("GET /window/open") >= 0) {
               Serial.println("GPIO 26 on");
+              if (digitalRead(motor) == HIGH) {
+                client.println("HIGH");
+                break;
+              }
               digitalWrite(motor, HIGH);
-            } else if (header.indexOf("GET /gate/close") >= 0) {
+            } else if (header.indexOf("GET /window/close") >= 0) {
+              if (digitalRead(motor) == LOW) {
+                client.println("LOW");
+                break;
+              }
               digitalWrite(motor, LOW);
             }
-            
-            // Display the HTML web page
-            client.println("<!DOCTYPE html><html>");
-            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-            client.println("<link rel=\"icon\" href=\"data:,\">");
-            // CSS to style the on/off buttons 
-            // Feel free to change the background-color and font-size attributes to fit your preferences
-            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-            client.println(".button2 {background-color: #555555;}</style></head>");
-            
-            // Web Page Heading
-            client.println("<body><h1>ESP32 Web Server</h1>");
-           
-            
-            // The HTTP response ends with another blank line
-            client.println();
-            // Break out of the while loop
+
+            client.println("ok");
+
             break;
-          } else { // if you got a newline, then clear currentLine
+          } else { 
             currentLine = "";
           }
-        } else if (c != '\r') {  // if you got anything else but a carriage return character,
-          currentLine += c;      // add it to the end of the currentLine
+        } else if (c != '\r') { 
+          currentLine += c;  
         }
       }
     }
-    // Clear the header variable
     header = "";
-    // Close the connection
     client.stop();
     Serial.println("Client disconnected.");
     Serial.println("");
